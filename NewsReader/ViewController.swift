2a1f9789f0bea4f98db72ab502b6c5ec4b7a85a9
Fernando Salvador <fsalvador.gestion@gmail.com>
2019-11-25 16:47:47 -0300

    Initial Commit

---
+++ NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Fernando on 25/11/2019.
+//  Copyright © 2019 Fernando Salvador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
